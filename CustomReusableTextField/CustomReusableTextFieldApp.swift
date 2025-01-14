5530a6211de02e404f59023c4190baf6004fa2c4
alsmhahmed <alsmh.ahmed@gen-c.com>
2025-01-14 15:01:50 +0200

    Initial Commit

---
+++ CustomReusableTextField/CustomReusableTextFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomReusableTextFieldApp.swift
+//  CustomReusableTextField
+//
+//  Created by alsmh ahmed on 14/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct CustomReusableTextFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
